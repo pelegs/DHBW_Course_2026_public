e6e1b4229968f56e287bc741418196d539f910eb
Peleg Sapir <peleg.sapir@senckenberg.de>
2026-05-20 21:22:51 +0200

    Completed graph generation script

--- code/page_rank/generate_dag.py
+++ code/page_rank/generate_dag.py
@@ -1,29 +1,50 @@
 import igraph as ig
 import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib as mpl
+from itertools import permutations
 
+# Load page links probability matrix
 PM = np.load("page_rank_matrix.npy")
-# num_pages = 10
+num_pages = PM.shape[0]
 # probs = np.ones(num_pages) / num_pages
 
-# First, we generate a random undirected graph with a fixed number of edges, without loops.
+# Create a Graph object from the probability matrix
 g = ig.Graph.Weighted_Adjacency(PM.tolist())
-
-# %%
-# Then we convert it to a DAG *in place*. This method samples DAGs with a given number of edges and vertices uniformly.
 g.to_directed(mode="acyclic")
 
-# %%
-# Finally, we can plot the graph using the Sugiyama layout from :meth:`igraph.Graph.layout_sugiyama`:
-# For edge width, check this out: https://stackoverflow.com/questions/23763382/igraph-set-edge-width-by-eid-python
+# Set edges witdths, using the following procedure:
+# 1. Create a default widths list (all ones)
+# 2. Create a list of indices 0,1,...,num_pages
+# 3. Using python's itertools permutations function,
+#    create a list of all pairs of nodes except those
+#    that where the two nodes are the same (e.g. (0,0), (1,1), etc.)
+# 4. Iterate over edges list and use the probability matrix to set the
+#    width of the respective edge (if it exsits, hence the try/except block)
+widths = np.ones(num_pages**2)
+idx_list = list(range(num_pages))
+edges = list(permutations(idx_list, 2))
+for src, dest in edges:
+    try:
+        id = g.get_eid(src, dest)
+        widths[id] = PM[src, dest]
+    except Exception:
+        print(f"No edge between vertices {src} and {dest}.")
+
+# Coloring the nodes using matplotlib's discrete color map
+cmap = mpl.colormaps["Paired"]
+colors = cmap(np.linspace(0, 1, num_pages)).tolist()
+
+# Actual plot
 fig, ax = plt.subplots()
 ig.plot(
     g,
     target=ax,
     layout="auto",
-    vertex_size=50,
-    vertex_color=["blue", "red", "green", "purple"],
+    vertex_size=75,
+    vertex_color=colors,
+    vertex_label=idx_list,
     edge_color="black",
-    edge_width=1,
+    edge_width=widths * 2,
 )
 plt.show()
